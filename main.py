3b2ce07ce09f292ef11cb056db144f5e227d0cd0
Luatop1 <timurr0830@gmail.com>
2025-04-27 13:27:16 +0300

    Changed  files

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def solve():
         calc.insert(0, "Ошибка")
 
 def add_num(btn):
-    if btn["text"] != "√":
+    if btn["text"] != "√" and btn["text"] != "π" and btn["text"] != "e" and btn["text"] != "φ":
         try:
             if calc.get()[len(calc.get()) - 1] == "(":
                 calc.insert(len(calc.get()), f"{btn["text"]})")
@@ -19,13 +19,20 @@ def add_num(btn):
                 calc.insert(len(calc.get()), btn["text"])
         except IndexError:
             calc.insert(len(calc.get()), btn["text"])
-    elif btn["text"] == "√":
-        calc.insert(len(calc.get()), "math.sqrt(")
+    else:
+        if btn["text"] == "√":
+            calc.insert(len(calc.get()), "math.sqrt(")
+        elif btn["text"] == "π":
+            calc.insert(len(calc.get()), "3.14159265359")
+        elif btn["text"] == "e":
+            calc.insert(len(calc.get()), "2.71828182846")
+        elif btn["text"] == "φ":
+            calc.insert(len(calc.get()), "1.6180339887")
 
 root = tk.Tk()
 
 root["bg"] = "#DADADA"
-root.geometry("190x170")
+root.geometry("190x212")
 root.resizable(False, False)
 
 calc = tk.Entry(root, borderwidth=4, width=29)
@@ -75,5 +82,20 @@ btn_minus.place(x=117, y=80)
 
 btn_sqrt = tk.Button(root, text="√", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_sqrt))
 btn_sqrt.place(x=117, y=125)
+
+btn_multipy = tk.Button(root, text="*", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_multipy))
+btn_multipy.place(x=117, y=170)
+
+btn_divide = tk.Button(root, text="/", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_divide))
+btn_divide.place(x=156, y=170)
+
+btn_pi = tk.Button(root, bg="#E5E5E5", text="π", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_pi))
+btn_pi.place(x=80, y=170)
+
+btn_eller = tk.Button(root, bg="#E5E5E5", text="e", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_eller))
+btn_eller.place(x=43, y=170)
+
+btn_f = tk.Button(root, bg="#E5E5E5", text="φ", height=1, width=2, font=("Arial", 15), command=lambda: add_num(btn_f))
+btn_f.place(x=5, y=170)
 
 root.mainloop()
